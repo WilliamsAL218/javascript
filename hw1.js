91510b5a3b7c79726a133aada5958bd81e0f2960
lorenzo <williams.rise44@gmail.com>
2021-04-28 11:20:24 -0400

    next

---
+++ hw1.js
@@ -0,0 +1,24 @@
+function greetUserByName(name) {
+    return `Whats up ${name}`;
+}
+
+  greetUserByName("John"); // returns "Hello Johnny" 
+
+
+// madlib
+
+function madlib('Anushka', 'art') {
+    var subject = 'art' ;
+    var name = 'Anushka' ;
+    var output = name + subject;
+    console.log(output);
+    return output;
+
+}
+
+
+function hello (name) {
+    var output = 'Hello ' + name;
+    console.log(output);
+    return output;
+  }
